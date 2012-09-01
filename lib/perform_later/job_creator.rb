204446aa259889ad90136c2917b873ecba905999
Gilad Zohari <gilad@ftbpro.com>
2012-09-01 20:53:25 +0300

    job creator - added splat to args

--- lib/perform_later/job_creator.rb
+++ lib/perform_later/job_creator.rb
@@ -17,7 +17,7 @@ module PerformLater
     end
 
     def enqueue(delay=nil)
-      return Resque.enqueue_in_with_queue(queue, delay, worker, klass_name, id, method, args) if delay 
+      return Resque.enqueue_in_with_queue(queue, delay, worker, klass_name, id, method, *args) if delay 
       Resque::Job.create(queue, worker, klass_name, id, method, *args)
     end
   end
